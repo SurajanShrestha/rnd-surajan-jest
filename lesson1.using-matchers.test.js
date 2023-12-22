4a6df9b381c2846e780cd289e5a541895ea2f168
Surajan Shrestha <surajanshrestha35@gmail.com>
2023-12-22 08:58:21 +0545

    feat: added matchers for undefined, null, numbers, floats, strings and arrays

--- lesson1.using-matchers.test.js
+++ lesson1.using-matchers.test.js
@@ -16,3 +16,74 @@ test("object check", () => {
 test("sum not equal to 0", () => {
   expect(sum(1, 0)).not.toBe(0);
 });
+
+// toBeNull, toBeUndefined, toBeDefined, toBeTruthy, toBeFalsy
+test("check null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).toBeFalsy();
+});
+
+test("check undefined", () => {
+  const n = undefined;
+  expect(n).toBeUndefined();
+  expect(n).toBeFalsy();
+});
+
+test("check zero", () => {
+  const n = 0;
+  expect(n).toBeDefined();
+  expect(n).toBeFalsy();
+});
+
+// Check numbers: toBeGreaterThan, toBeGreaterThanOrEqual, toBeLessThan, toBeLessThanOrEqual, toBe and toEqual
+test("check numbers", () => {
+  const total = sum(1, 2);
+  expect(total).toBeGreaterThan(2);
+  expect(total).toBeGreaterThanOrEqual(3);
+  expect(total).toBeLessThan(4);
+  expect(total).toBeLessThanOrEqual(5);
+});
+
+// Check floating numbers: toBeCloseTo might be better than toEqual, because of probability of small floating point differences
+test("check floating numbers", () => {
+  const total = sum(0.2, 0.1);
+  // 👇 This fails because in JavaScript, 0.2 + 0.1 is actually 0.30000000000000004.
+  // expect(total).toBe(0.3);
+
+  // This passes
+  expect(total).toBeCloseTo(0.3);
+
+  // By default, toBeCloseTo accepts 2nd argument:numDigits that checks precision i.e. num of digits after decimal point.
+  // Be default, numDigits has value of 2 i.e. accepts difference <0.005. But we also can change it.
+  // Check 👉: https://jestjs.io/docs/expect#tobeclosetonumber-numdigits
+  expect(total).toBeCloseTo(0.3, 5);
+});
+
+// Check strings: toMatch
+test("check strings", () => {
+  const message = "Jest";
+
+  // Match exact string
+  expect(message).toMatch("Jest");
+
+  // Match a regexp i.e. see if message contains certain strings
+  expect(message).toMatch(/J/);
+  expect(message).toMatch(/st/);
+
+  // Not match: Is Case Sensitive
+  expect(message).not.toMatch(/j/);
+  expect(message).not.toMatch(/stop/);
+});
+
+// Check arrays: toContain
+test("check arrays", () => {
+  const arr = ["apple", "mango", "banana", "orange", "grape"];
+
+  // Contains
+  expect(arr).toContain("apple");
+
+  // Not Contain
+  expect(arr).not.toContain("watermelon");
+});
