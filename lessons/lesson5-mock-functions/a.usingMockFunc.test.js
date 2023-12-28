817134cdc7912208231a682267e96767d6d672e6
Surajan Shrestha <surajanshrestha35@gmail.com>
2023-12-28 20:13:13 +0545

    feat: mock functions basics

---
+++ lessons/lesson5-mock-functions/a.usingMockFunc.test.js
@@ -0,0 +1,21 @@
+const { forEach } = require("./forEach");
+
+// Creating a mock function
+const mockCallback = jest.fn((x) => 10 + x);
+
+test("forEach mock function", () => {
+  forEach([1, 2], mockCallback);
+
+  console.log(mockCallback.mock.calls); // Prints 👉: [ [ 1 ], [ 2 ] ]
+  console.log(mockCallback.mock.calls[0]); // Prints 👉: [ 1 ]
+  console.log(mockCallback.mock.calls[0][0]); // Prints 👉: 1
+
+  // Testing that this mock function is called twice
+  expect(mockCallback.mock.calls).toHaveLength(2);
+
+  // Testing if 1st argument of the 1st call of this mock function is 1
+  expect(mockCallback.mock.calls[0][0]).toBe(1);
+
+  // Testing if 1st argument of the 1st call of this mock function is 1
+  expect(mockCallback.mock.calls[1][0]).toBe(2);
+});
